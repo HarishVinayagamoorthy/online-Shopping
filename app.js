5a8846905bcb17c41f1747c678563871a5828b35
HarishVinayagamoorthy <harishvinayagamoorthy123@gmail.com>
2024-01-29 15:08:52 +0530

    updated

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ app.use(cors());
 app.use('/api/v1/',products);
 app.use('/api/v1/',orders);
 
-if (process.env.NODE_ENV == 'production') {
-    app.use(express.static(path.join(__dirname, '..', 'frontend',  'build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'))
-    });
-}
+// if (process.env.NODE_ENV == 'production') {
+//     app.use(express.static(path.join(__dirname, '..', 'frontend',  'build')));
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'))
+//     });
+// }
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV}`)
