04a5b50d80319097883f2a7accde0662c315c2f5
HarishVinayagamoorthy <harishvinayagamoorthy123@gmail.com>
2024-01-29 17:28:46 +0530

    code updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,46 @@
 
 
+// import React, { Fragment, useEffect, useState } from 'react';
+// import ProductCard from '../components/ProductCard';
+// import Spinner from '../components/Spinner'; // Import the Spinner component
+// import { useSearchParams } from 'react-router-dom';
+
+// export default function Home() {
+//     const [products, setProducts] = useState([]);
+//     const [loading, setLoading] = useState(true);
+//     const [searchParams, setSearchParams] = useSearchParams();
+
+//     useEffect(() => {
+//         setLoading(true);
+//         fetch(process.env.REACT_APP_API_URL + '/products?' + searchParams)
+//             .then(res => res.json())
+//             .then(res => {
+//                 setProducts(res.products);
+//                 setLoading(false);
+//             });
+//     }, [searchParams]);
+
+//     return (
+//         <Fragment>
+//             <h1 id="products_heading">Latest Products</h1>
+
+//             {loading ? (
+//                 <Spinner /> // Use the Spinner component for loading state
+//             ) : (
+//                 <section id="products" className="container mt-5">
+//                     <div className="row">
+//                         {products.map(product => (
+//                             <ProductCard key={product.id} product={product} />
+//                         ))}
+//                     </div>
+//                 </section>
+//             )}
+//         </Fragment>
+//     );
+// }
+
+
+// Home.js
 import React, { Fragment, useEffect, useState } from 'react';
 import ProductCard from '../components/ProductCard';
 import Spinner from '../components/Spinner'; // Import the Spinner component
@@ -30,7 +71,7 @@ export default function Home() {
                 <section id="products" className="container mt-5">
                     <div className="row">
                         {products.map(product => (
-                            <ProductCard key={product.id} product={product} />
+                            <ProductCard key={product._id} product={product} />
                         ))}
                     </div>
                 </section>
