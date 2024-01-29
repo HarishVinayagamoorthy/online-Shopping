8411686b7695d9b5d188b42a53e4bbbf4786bd3e
HarishVinayagamoorthy <harishvinayagamoorthy123@gmail.com>
2024-01-29 17:38:47 +0530

    updated

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,151 +1,48 @@
-// import {useState, useEffect} from 'react';
-// import { useParams } from 'react-router-dom';
-// import {toast} from 'react-toastify';
-// import Spinner from '../components/Spinner'; // Import the Spinner component
-
-
-// export default function ProductDetail({cartItems, setCartItems}) {
-//     const [product, setProduct] = useState(null);
-//     const [qty, setQty] = useState(1);
-//     const {id} = useParams();
-
-//     useEffect(() => {
-//         fetch(process.env.REACT_APP_API_URL+'/product/'+id)
-//         .then(res => res.json())
-//         .then( res => setProduct(res.product))
-//     },[])
-
-//     function addToCart() {
-//         const itemExist = cartItems.find((item) => item.product._id == product._id)
-//         if (!itemExist) {
-//             const newItem = {product, qty};
-//             setCartItems((state) => [...state, newItem]);
-//             toast.success("Cart Item added succesfully!")
-//         }
-//     }
-
-//     function increaseQty() {
-//         if (product.stock == qty) {
-//             return;
-//         }
-//         setQty((state) => state + 1);
-//     }
-
-//     function decreaseQty() {
-//         if (qty > 1) {
-//             setQty((state) => state - 1);
-//         }
-//     }
-
-
-//     return  product && <div className="container container-fluid">
-//                 <div className="row f-flex justify-content-around">
-//                     <div className="col-12 col-lg-5 img-fluid" id="product_image">
-//                         <img src={product.images[0].image} alt="sdf" height="500" width="500" />
-//                     </div>
-
-//                     <div className="col-12 col-lg-5 mt-5">
-//                         <h3>{product.name}</h3>
-//                         <p id="product_id">Product #{product._id}</p>
-
-//                         <hr />
-
-//                         <div className="rating-outer">
-//                             <div className="rating-inner" style={{width : `${product.ratings/5 * 100}%`}}></div>
-//                         </div>
-                
-
-//                         <hr />
-
-//                         <p id="product_price">${product.price}</p>
-//                         <div className="stockCounter d-inline">
-//                             <span className="btn btn-danger minus" onClick={decreaseQty}>-</span>
-
-//                             <input type="number" className="form-control count d-inline" value={qty} readOnly />
-
-//                             <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
-//                         </div>
-//                         <button type="button" onClick={addToCart} disabled={product.stock == 0}   id="cart_btn" className="btn btn-primary d-inline ml-4">Add to Cart</button>
-
-//                         <hr />
-
-//                         <p>Status: <span id="stock_status" className={product.stock > 0 ?'text-success':'text-danger'}>{product.stock > 0  ?'In Stock' : 'Out of Stock'}</span></p>
-
-//                         <hr />
-
-//                         <h4 className="mt-2">Description:</h4>
-//                         <p>{product.description}</p>
-//                         <hr />
-//                         <p id="product_seller mb-3">Sold by: <strong>{product.seller}</strong></p>
-                        
-//                         <div className="rating w-50"></div>
-                                
-//                     </div>
-
-//                 </div>
-//             </div>
-// }
-
-
-
-
-// ProductDetail.js
-
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import Spinner from '../components/Spinner';
+import {toast} from 'react-toastify';
 
-export default function ProductDetail({ cartItems, setCartItems }) {
+export default function ProductDetail({cartItems, setCartItems}) {
     const [product, setProduct] = useState(null);
     const [qty, setQty] = useState(1);
-    const { id } = useParams();
+    const {id} = useParams();
 
     useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}/product/${id}`)
-            .then(res => res.json())
-            .then(res => setProduct(res.product))
-            .catch(error => {
-                console.error('Error fetching product details:', error);
-                // Handle error, e.g., display an error message or redirect to an error page
-            });
-    }, [id]);
+        fetch(process.env.REACT_APP_API_URL+'/product/'+id)
+        .then(res => res.json())
+        .then( res => setProduct(res.product))
+    },[])
 
     function addToCart() {
-        const itemExist = cartItems.find(item => item.product._id === product?._id);
+        const itemExist = cartItems.find((item) => item.product._id == product._id)
         if (!itemExist) {
-            const newItem = { product, qty };
-            setCartItems(state => [...state, newItem]);
-            toast.success('Cart Item added successfully!');
+            const newItem = {product, qty};
+            setCartItems((state) => [...state, newItem]);
+            toast.success("Cart Item added succesfully!")
         }
     }
 
     function increaseQty() {
-        if (product?.stock === qty) {
+        if (product.stock == qty) {
             return;
         }
-        setQty(state => state + 1);
+        setQty((state) => state + 1);
     }
 
     function decreaseQty() {
         if (qty > 1) {
-            setQty(state => state - 1);
+            setQty((state) => state - 1);
         }
     }
 
-    return product ? (
-        <div className="container container-fluid">
-            <div className="row f-flex justify-content-around">
-                <div className="col-12 col-lg-5 img-fluid" id="product_image">
-                    <img
-                        src={product?.images[0]?.image}
-                        alt={product?.name || 'Product Image'}
-                        height="500"
-                        width="500"
-                    />
-                </div>
 
-                <div className="col-12 col-lg-5 mt-5">
+    return  product && <div className="container container-fluid">
+                <div className="row f-flex justify-content-around">
+                    <div className="col-12 col-lg-5 img-fluid" id="product_image">
+                        <img src={product.images[0].image} alt="sdf" height="500" width="500" />
+                    </div>
+
+                    <div className="col-12 col-lg-5 mt-5">
                         <h3>{product.name}</h3>
                         <p id="product_id">Product #{product._id}</p>
 
@@ -182,22 +79,7 @@ export default function ProductDetail({ cartItems, setCartItems }) {
                         <div className="rating w-50"></div>
                                 
                     </div>
+
+                </div>
             </div>
-        </div>
-    ) : (
-        <Spinner />
-    );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
